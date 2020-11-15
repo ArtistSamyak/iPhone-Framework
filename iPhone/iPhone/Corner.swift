1b758eb644eac048b405450319486fb77e6794b2
ArtistSamyak <67262220+ArtistSamyak@users.noreply.github.com>
2020-11-15 12:03:48 +0530

    Add files via upload

---
+++ iPhone/iPhone/Corner.swift
@@ -0,0 +1,15 @@
+//
+//  Corner.swift
+//  iPhone
+//
+//  Created by Samyak Pawar on 15/11/20.
+//
+
+import Foundation
+import UIKit
+
+public struct Corner {
+    public static func round(view:UIView) {
+        view.layer.cornerRadius = 20
+    }
+}
---
+++ iPhone/iPhone/iPhone.h
@@ -0,0 +1,18 @@
+//
+//  iPhone.h
+//  iPhone
+//
+//  Created by Samyak Pawar on 15/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iPhone.
+FOUNDATION_EXPORT double iPhoneVersionNumber;
+
+//! Project version string for iPhone.
+FOUNDATION_EXPORT const unsigned char iPhoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iPhone/PublicHeader.h>
+
+
